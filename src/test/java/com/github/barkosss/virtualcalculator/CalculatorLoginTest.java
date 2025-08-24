21a556ad373b26ef82284ed88bbf022721ecb303
Andrey Baryshev <andrey.bar66@gmail.com>
2025-08-25 02:26:15 +0500

    Add new tests

--- src/test/java/com/github/barkosss/virtualcalculator/CalculatorLoginTest.java
+++ src/test/java/com/github/barkosss/virtualcalculator/CalculatorLoginTest.java
@@ -58,12 +58,162 @@ public class CalculatorLoginTest {
         assert result == null; // true
     }
 
-    @Test // TODO: Negative
+    @Test
     void fourthCorrectBracketsPositive() {
         String expression = "((-1) * -5 * 2)";
         Double result = CalculatorLogic.execute(expression);
 
         assert result != null; // true
         assert Objects.equals(result, 10.0); // true
+    }
+
+    @Test
+    void firstTestPositive() {
+        String expression = "(5 * 3 + ( -5 * 5 ) * 7 + 5)";
+        Double result = CalculatorLogic.execute(expression);
+
+        assert result != null; // true
+        assert Objects.equals(result, -155.0); // true
+    }
+
+    @Test
+    void secondTestNegative() {
+        String expression = "5 * -3 / 4 * -7)";
+        Double result = CalculatorLogic.execute(expression);
+
+        assert result == null; // true
+    }
+
+    @Test
+    void thirdTestNegative() {
+        String expression = "(5 * 3( * )7 +5)";
+        Double result = CalculatorLogic.execute(expression);
+
+        assert result == null; // true
+    }
+
+    @Test
+    void fourthTestNegative() {
+        String expression = "() 5 + 5";
+        Double result = CalculatorLogic.execute(expression);
+
+        assert result == null; // true
+    }
+
+    @Test
+    void fifthTestNegative() {
+        String expression = "(5 + 5) * 2s";
+        Double result = CalculatorLogic.execute(expression);
+
+        assert result == null; // true
+    }
+
+    @Test
+    void sixthTestNegative() {
+        String expression = "(5 + 5) * 2 +";
+        Double result = CalculatorLogic.execute(expression);
+
+        assert result == null; // true
+    }
+
+    @Test
+    void seventhTestPositive() {
+        String expression = "-5 * 3";
+        Double result = CalculatorLogic.execute(expression);
+
+        assert result != null; // true
+        assert Objects.equals(result, -15.0); // true
+    }
+
+    @Test
+    void eighthTestPositive() {
+        String expression = "(5 + 2 - 1) * (5 + 2)";
+        Double result = CalculatorLogic.execute(expression);
+
+        assert result != null; // true
+        assert Objects.equals(result, 42.0); // true
+    }
+
+    @Test
+    void ninthTestNegative() {
+        String expression = "(5 * 3( * )7 +5)";
+        Double result = CalculatorLogic.execute(expression);
+
+        assert result == null; // true
+    }
+
+    @Test
+    void tenthTestPositive() {
+        String expression = "(5 * 3 + ( -5 * 5 ) * 7 + 5)";
+        Double result = CalculatorLogic.execute(expression);
+
+        assert result != null; // true
+        assert Objects.equals(result, -155.0); // true
+    }
+
+    @Test
+    void twelfthTestPositive() {
+        String expression = "(((((((5 * (-3))))))))";
+        Double result = CalculatorLogic.execute(expression);
+
+        assert result != null; // true
+        assert Objects.equals(result, -15.0); // true
+    }
+
+    @Test
+    void twentiethTestPositive() {
+        String expression = "(5 * 3 + ( -5 * 5 ) * 7 + 5)";
+        Double result = CalculatorLogic.execute(expression);
+
+        assert result != null; // true
+        assert Objects.equals(result, -155.0); // true
+    }
+
+    @Test
+    void thirtiethTestNegative() {
+        String expression = "(5 + 5) * 2s";
+        Double result = CalculatorLogic.execute(expression);
+
+        assert result == null; // true
+    }
+
+    @Test
+    void fortiethTestNegative() {
+        String expression = "(5 + 5) * 2 +";
+        Double result = CalculatorLogic.execute(expression);
+
+        assert result == null; // true
+    }
+
+    @Test
+    void fiftiethTestNegative() {
+        String expression = "() 5 + 5";
+        Double result = CalculatorLogic.execute(expression);
+
+        assert result == null; // true
+    }
+
+    @Test
+    void sixtiethTestNegative() {
+        String expression = "(1 + 1 * 5 + 5 + 2 * 2";
+        Double result = CalculatorLogic.execute(expression);
+
+        assert result == null; // true
+    }
+
+    @Test
+    void seventiethTestNegative() {
+        String expression = "(1 + 1 * 5( + 5) +) 2 * 2";
+        Double result = CalculatorLogic.execute(expression);
+
+        assert result == null; // true
+    }
+
+    @Test
+    void eightiethTestNegative() {
+        String expression = "(1 + 1 * (5 + 5) +) 2 * 2";
+        Double result = CalculatorLogic.execute(expression);
+
+        assert result == null; // true
     }
 }
